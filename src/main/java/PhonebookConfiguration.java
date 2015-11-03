46d4af61878bdf94b32a5da678400e9600aacbd6
Andrei Petric <andrei@theappbusiness.com>
2015-11-03 14:22:43 +0000

    Configuration validation + optional configuration parameter (additionalMessage)

--- src/main/java/PhonebookConfiguration.java
+++ src/main/java/PhonebookConfiguration.java
@@ -1,15 +1,23 @@
 import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.Max;
 import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
 
 /**
  * Created by andrei on 03/11/15.
  */
 public class PhonebookConfiguration extends Configuration {
     @JsonProperty
+    @NotEmpty
     private String message;
 
     @JsonProperty
+    @Max(10)
     private int messageRepetitions;
+
+    @JsonProperty
+    private String additionalMessage = "This is optional";
 
     public String getMessage() {
         return message;
@@ -17,5 +25,9 @@ public class PhonebookConfiguration extends Configuration {
 
     public int getMessageRepetitions() {
         return messageRepetitions;
+    }
+
+    public String getAdditionalMessage() {
+        return additionalMessage;
     }
 }
